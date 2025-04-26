4034ae3ceed4f235408a2027d82a7e5483e5b941
Svitlana Didkivska <zzzdafzzz@gmail.com>
2025-04-26 18:51:10 +0200

    Created a To-do list model and a migration for it (using python3 manage.py makemigrations)

--- webapp/todo_lists/models.py
+++ webapp/todo_lists/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User # new
+ 
+ # Create your models here.
+class TodoList(models.Model):
+    name = models.CharField('To-do list name', max_length=100, null=False, blank=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
