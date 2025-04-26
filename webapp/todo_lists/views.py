dbf958872a126ed219bdb1fdbe2a267a84eee9b2
Svitlana Didkivska <zzzdafzzz@gmail.com>
2025-04-26 18:39:31 +0200

    Added an empty Create form

--- webapp/todo_lists/views.py
+++ webapp/todo_lists/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
  # Create your views here.
 def index(request):
     return render(request, 'todo_lists/index.html')
+
+def create(request):
+    return render(request, 'todo_lists/create.html')
