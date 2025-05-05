cc8f80e3b3663e88ad37e93890eb323cef93e9a4
SvitlanaDidkivska <zzzdafzzz@gmail.com>
2025-05-05 12:21:45 +0200

    Dodano model i migrację do zadań

--- webapp/todo_lists/models.py
+++ webapp/todo_lists/models.py
@@ -9,3 +9,10 @@ class TodoList(models.Model):
     name = models.CharField('To-do list name', max_length=100, null=False, blank=False)
     # Dokumentacja z przykładem relacji jeden-do-wielu: https://docs.djangoproject.com/en/5.1/topics/db/examples/many_to_one/
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Task(models.Model):
+     name = models.CharField('Task name', max_length=100, null=False, blank=False)
+     # Lista wszystich dostępnych typów danych dostępna jest w dokumentacji: https://docs.djangoproject.com/en/5.2/ref/models/fields/#field-types
+     is_completed = models.BooleanField('Task completion indicator', default=False, null=False, blank=False)
+     # Dokumentacja z przykładem relacji jeden-do-wielu: https://docs.djangoproject.com/en/5.1/topics/db/examples/many_to_one/
+     todo_list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
