a74822834e26c988bd0bed3566f3eb8848ab0ba3
SvitlanaDidkivska <zzzdafzzz@gmail.com>
2025-05-05 13:14:06 +0200

    Model TodoList poszerzono o o metody sprawdzania wykonania wszystkich zadań

--- webapp/todo_lists/models.py
+++ webapp/todo_lists/models.py
@@ -9,6 +9,11 @@ class TodoList(models.Model):
     name = models.CharField('To-do list name', max_length=100, null=False, blank=False)
     # Dokumentacja z przykładem relacji jeden-do-wielu: https://docs.djangoproject.com/en/5.1/topics/db/examples/many_to_one/
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    def is_completed(self):
+        return not self.task_set.filter(is_completed=False).exists()
+ 
+    def is_empty(self):
+        return not self.task_set.all().exists()
 
 class Task(models.Model):
      name = models.CharField('Task name', max_length=100, null=False, blank=False)
