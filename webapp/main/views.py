b9216ff463f09fbbb62b390e003fb42318973ab8
Aleksandra Korniak <ola.korniak@gmail.com>
2025-06-05 20:49:59 +0200

    Links to img (#29)
    * added the offers to the homepage
    
    * added no-image-available.png and fixed the height somewhat
    
    * fixed the formatting

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,10 +1,12 @@
 # Plik do definiowania widoków, które są renderowane za pomocą szablonizatora Jinja oraz wyświetlane w przeglądarce
 
 from django.shortcuts import render
+from marketplace.models import Posts
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    posts = Posts.objects.all().order_by('-published_at')[:3]
+    return render(request, 'main/index.html', {'posts': posts})
 
 def about(request):
     return render(request, 'main/about.html')
